5a161acb2332cd6f34bb3d08312dda7ce8ce55e6
auxeva <solia16@hotmail.com>
2015-08-13 21:36:03 -0300

    agregado control de usuario ABM

---
+++ Controles/ctrlABM.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Controles
+{
+    partial class ctrlABM
+    {
+        /// <summary> 
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar 
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnModificacion = new System.Windows.Forms.Button();
+            this.btnBaja = new System.Windows.Forms.Button();
+            this.btnAlta = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.Location = new System.Drawing.Point(79, 3);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(118, 42);
+            this.btnAceptar.TabIndex = 9;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(219, 3);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(118, 42);
+            this.btnCancelar.TabIndex = 8;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnModificacion
+            // 
+            this.btnModificacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificacion.Location = new System.Drawing.Point(281, 3);
+            this.btnModificacion.Name = "btnModificacion";
+            this.btnModificacion.Size = new System.Drawing.Size(133, 42);
+            this.btnModificacion.TabIndex = 7;
+            this.btnModificacion.Text = "Modificacion";
+            this.btnModificacion.UseVisualStyleBackColor = true;
+            this.btnModificacion.Click += new System.EventHandler(this.btnModificacion_Click);
+            // 
+            // btnBaja
+            // 
+            this.btnBaja.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBaja.Location = new System.Drawing.Point(142, 3);
+            this.btnBaja.Name = "btnBaja";
+            this.btnBaja.Size = new System.Drawing.Size(133, 42);
+            this.btnBaja.TabIndex = 6;
+            this.btnBaja.Text = "Baja";
+            this.btnBaja.UseVisualStyleBackColor = true;
+            this.btnBaja.Click += new System.EventHandler(this.btnBaja_Click);
+            // 
+            // btnAlta
+            // 
+            this.btnAlta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlta.Location = new System.Drawing.Point(3, 3);
+            this.btnAlta.Name = "btnAlta";
+            this.btnAlta.Size = new System.Drawing.Size(133, 42);
+            this.btnAlta.TabIndex = 5;
+            this.btnAlta.Text = "Alta";
+            this.btnAlta.UseVisualStyleBackColor = true;
+            this.btnAlta.Click += new System.EventHandler(this.btnAlta_Click);
+            // 
+            // ctrlABM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnModificacion);
+            this.Controls.Add(this.btnBaja);
+            this.Controls.Add(this.btnAlta);
+            this.Name = "ctrlABM";
+            this.Size = new System.Drawing.Size(417, 48);
+            this.Load += new System.EventHandler(this.ctrlABM_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnModificacion;
+        private System.Windows.Forms.Button btnBaja;
+        private System.Windows.Forms.Button btnAlta;
+    }
+}
---
+++ Controles/ctrlABM.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Controles
+{
+    public partial class ctrlABM : UserControl
+    {
+
+        public delegate void uctrlABMEventHandler(Object sender, ref bool cancela);
+        public delegate void uctrlABMEventHandler2(Object sender);
+
+        public event uctrlABMEventHandler Aceptar;
+        public event uctrlABMEventHandler2 Cancelar;
+        public event uctrlABMEventHandler2 Alta;
+        public event uctrlABMEventHandler2 Baja;
+        public event uctrlABMEventHandler2 Modificacion;
+
+        protected virtual void onModificacion()
+        {
+            if (Modificacion != null)
+            {
+                Modificacion(this);
+                opcion(false);
+            }
+        }
+
+        protected virtual void onBaja()
+        {
+            if (Baja != null)
+            {
+                Baja(this);
+                opcion(false);
+            }
+        }
+
+        protected virtual void onAlta()
+        {
+            if (Alta != null)
+            {
+                Alta(this);
+                opcion(false);
+            }
+        }
+
+        protected virtual void onCancelar()
+        {
+            if (Cancelar != null)
+            {
+                Cancelar(this);
+                opcion(true);
+            }
+        }
+
+        protected virtual void onAceptar()
+        {
+            bool x = false;
+            if (Aceptar != null)
+            {
+                Aceptar(this, ref x);
+                opcion(x);
+            }
+        }
+
+        public ctrlABM()
+        {
+            InitializeComponent();
+        }
+
+        private void ctrlABM_Load(object sender, EventArgs e)
+        {
+            opcion(true);
+        }
+
+        private void opcion(bool x)
+        {
+            btnAlta.Visible = x;
+            btnBaja.Visible = x;
+            btnModificacion.Visible = x;
+            btnAceptar.Visible = !x;
+            btnCancelar.Visible = !x;
+        }
+
+        private void btnAlta_Click(object sender, EventArgs e)
+        {
+            onAlta();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            onAceptar();
+        }
+
+        private void btnBaja_Click(object sender, EventArgs e)
+        {
+            onBaja();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            onCancelar();
+        }
+
+        private void btnModificacion_Click(object sender, EventArgs e)
+        {
+            onModificacion();
+        }
+
+    }
+}
