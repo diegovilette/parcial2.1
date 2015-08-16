06c492b811d0788c5099dae3ff996bf9488f18d6
smatiassosa <sosa.sergiomatias@gmail.com>
2015-08-15 23:39:44 -0300

    Factura A

--- WindowsFormsApplication2/frmTicket.cs
+++ WindowsFormsApplication2/frmTicket.cs
@@ -37,23 +37,31 @@ namespace WindowsFormsApplication2
         {
             DataTable dt = new DataTable();
             dt.Columns.Add("Producto", typeof(String));
-            dt.Columns.Add("Cantidad", typeof(String));
-            dt.Columns.Add("Total", typeof(String));
+            dt.Columns.Add("Cantidad", typeof(int));
+            dt.Columns.Add("Precio Unitario", typeof(double));
+            dt.Columns.Add("Total", typeof(double));
 
-
+            double total = 0;
             foreach (Producto p in lpr)
             {
-                dt.Rows.Add(p.Descripcion, p.Stock, ((p.PrecioCosto *(1 +  p.CoefUtil)) * p.Stock));
+                dt.Rows.Add(p.Descripcion, p.Stock,p.PrecioCosto, p.PrecioCosto);
+                total += p.PrecioCosto *(1 +  p.CoefUtil) * p.Stock;
+
             }
 
             if (tipoF == "A")
             {
+                string iva = "21";
                 crFactura cr = new crFactura();
-
-                cr.SetParameterValue("parNombreCliente", cliente);
-                crFacturaA.ReportSource = cr;
-
                 cr.SetDataSource(dt);
+                cr.SetParameterValue("CuitCliente", "12312");
+                cr.SetParameterValue("parNombreCliente", cliente);
+                cr.SetParameterValue("idFactura", "2");
+                cr.SetParameterValue("TotalIVA", total);
+                cr.SetParameterValue("IVA", iva);
+                crFacturaA.ReportSource = cr;
+                
+                
             }
             else 
             {
