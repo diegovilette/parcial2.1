5fa04c09bc0ee3b22c9b7e9c1d52e19d521b2b22
Diego <diegov.0417@gmail.com>
2015-08-16 00:15:36 -0300

    paso la factura y no solo el tipo

--- WindowsFormsApplication2/frmRegistrarVenta.cs
+++ WindowsFormsApplication2/frmRegistrarVenta.cs
@@ -163,7 +163,7 @@ namespace WindowsFormsApplication2
 
                         
                             //imprime
-                            frmTicket tic = new frmTicket(lista, venta.Id, cliente, "A");
+                            frmTicket tic = new frmTicket(lista, venta.Id, cliente, factura);
                             tic.ShowDialog();
                         
                     }
@@ -172,7 +172,7 @@ namespace WindowsFormsApplication2
                         Agrega.Venta(venta, lista, factura, false);
                        
                             //imprime
-                            frmTicket tic = new frmTicket(lista, venta.Id, cliente, "B");
+                            frmTicket tic = new frmTicket(lista, venta.Id, cliente, factura);
                             tic.ShowDialog();
                         
                     }
