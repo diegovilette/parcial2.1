e659031498c1c4cb9cec067687edc58152f4e536
maximorero14 <maximorero14@gmail.com>
2015-08-18 13:02:38 -0300

    Se desvincularon las funciones de combo box y calendar

--- WindowsFormsApplication2/frmGraficas.Designer.cs
+++ WindowsFormsApplication2/frmGraficas.Designer.cs
@@ -48,18 +48,18 @@
             // 
             this.cbxSucursal.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbxSucursal.FormattingEnabled = true;
-            this.cbxSucursal.Location = new System.Drawing.Point(20, 4);
-            this.cbxSucursal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbxSucursal.Location = new System.Drawing.Point(15, 3);
             this.cbxSucursal.Name = "cbxSucursal";
-            this.cbxSucursal.Size = new System.Drawing.Size(255, 24);
+            this.cbxSucursal.Size = new System.Drawing.Size(192, 21);
             this.cbxSucursal.TabIndex = 0;
+            this.cbxSucursal.SelectedIndexChanged += new System.EventHandler(this.cbxSucursal_SelectedIndexChanged);
             // 
             // calGrafica
             // 
-            this.calGrafica.Location = new System.Drawing.Point(15, 31);
-            this.calGrafica.Margin = new System.Windows.Forms.Padding(12, 11, 12, 11);
+            this.calGrafica.Location = new System.Drawing.Point(11, 25);
             this.calGrafica.Name = "calGrafica";
             this.calGrafica.TabIndex = 1;
+            this.calGrafica.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.calGrafica_DateChanged);
             // 
             // gCategorias
             // 
@@ -67,15 +67,14 @@
             this.gCategorias.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.gCategorias.Legends.Add(legend1);
-            this.gCategorias.Location = new System.Drawing.Point(651, 260);
-            this.gCategorias.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gCategorias.Location = new System.Drawing.Point(488, 211);
             this.gCategorias.Name = "gCategorias";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.gCategorias.Series.Add(series1);
-            this.gCategorias.Size = new System.Drawing.Size(543, 369);
+            this.gCategorias.Size = new System.Drawing.Size(407, 300);
             this.gCategorias.TabIndex = 2;
             this.gCategorias.Text = "chart1";
             // 
@@ -85,14 +84,13 @@
             this.gVentas.ChartAreas.Add(chartArea2);
             legend2.Name = "Legend1";
             this.gVentas.Legends.Add(legend2);
-            this.gVentas.Location = new System.Drawing.Point(13, 257);
-            this.gVentas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gVentas.Location = new System.Drawing.Point(10, 209);
             this.gVentas.Name = "gVentas";
             series2.ChartArea = "ChartArea1";
             series2.Legend = "Legend1";
             series2.Name = "Series1";
             this.gVentas.Series.Add(series2);
-            this.gVentas.Size = new System.Drawing.Size(543, 369);
+            this.gVentas.Size = new System.Drawing.Size(407, 300);
             this.gVentas.TabIndex = 3;
             this.gVentas.Text = "chart2";
             // 
@@ -101,22 +99,22 @@
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel1.Controls.Add(this.cbxSucursal);
             this.panel1.Controls.Add(this.calGrafica);
-            this.panel1.Location = new System.Drawing.Point(454, 5);
+            this.panel1.Location = new System.Drawing.Point(340, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(298, 245);
+            this.panel1.Size = new System.Drawing.Size(224, 200);
             this.panel1.TabIndex = 4;
             // 
             // frmGraficas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1207, 640);
+            this.ClientSize = new System.Drawing.Size(905, 520);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.gVentas);
             this.Controls.Add(this.gCategorias);
             this.ForeColor = System.Drawing.Color.White;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmGraficas";
             this.Text = "Graficos";
             this.Load += new System.EventHandler(this.frmGraficas_Load);
